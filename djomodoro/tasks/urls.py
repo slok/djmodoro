de1b67f31a1726a36b5c65300f6537c660e4c9cf
Xabier Larrakoetxea <slok69@gmail.com>
2014-12-18 22:21:31 +0100

    Create tasks made

--- djomodoro/tasks/urls.py
+++ djomodoro/tasks/urls.py
@@ -1,7 +1,10 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 
 from . import views
 
 urlpatterns = patterns('',
     url(r'^$', views.IndexView.as_view(), name="index"),
+    url(r'^task/create$', views.NewTaskView.as_view(), name="task_create"),
+    #url(r'^$', views.IndexView.as_view(), name="list-tasks"),
+    #url(r'^$', views.IndexView.as_view(), name="list-runs"),
 )
--- djomodoro/tasks/views.py
+++ djomodoro/tasks/views.py
@@ -1,5 +1,25 @@
 from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.core.urlresolvers import reverse_lazy
+from django.utils.translation import ugettext_lazy as _
+from django.db.models import Count
+
+from .models import Task
 
 
 class IndexView(TemplateView):
     template_name = "tasks/task_index.html"
+
+
+class NewTaskView(SuccessMessageMixin, CreateView):
+    model = Task
+    fields = ['name', 'description']
+    template_name = "tasks/task_task_create.html"
+    success_url = reverse_lazy("tasks:task_create")
+    success_message = _("%(name)s was created successfully")
+
+    def get_context_data(self, **kwargs):
+        kwargs['task_list'] = Task.objects.annotate(
+                                Count('run')).order_by('id')[:5]
+        return super().get_context_data(**kwargs)  # Python3 style super
