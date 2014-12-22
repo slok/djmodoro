7c08b2e6ac8cad98af2485ef984623da6ebf7b97
Xabier Larrakoetxea <slok69@gmail.com>
2014-12-22 21:22:42 +0100

    Add momentjs

--- djomodoro/djomodoro/settings/base.py
+++ djomodoro/djomodoro/settings/base.py
@@ -126,7 +126,8 @@ PIPELINE_JS = {
     'libs': {
         'source_filenames': (
             'bower/jquery/dist/jquery.js',
-            'bower/jquery.countdown/dist/jquery.countdown.js'
+            'bower/jquery.countdown/dist/jquery.countdown.js',
+            'bower/moment/moment.js'
         ),
         'output_filename': 'js/libs.min.js',
     }
